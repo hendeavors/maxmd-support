31b71b39340e4f133dc50608edee0998ea3ec7d4
Adam Rodriguez <adam_rodriguez@bitbucket.org>
2017-10-26 10:03:27 -0700

    facade access to rest and soap clients

---
+++ src/Client.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Endeavors\MaxMD\Support;
+
+// todo include a proofingsoap client
+final class Client
+{
+    final public static function DirectMessage()
+    {
+        return DirectMessageSoapClient::getInstance();
+    }
+
+    final public static function ProofingRest()
+    {
+        return ProofingRestClient::getInstance();
+    }
+
+    final public static function PatientRegistration()
+    {
+        return PatientRegistrationSoapClient::getInstance();
+    }
+}
