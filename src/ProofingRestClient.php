680ba3076815b7bd37d695b99fc6b84191fa0688
Adam Rodriguez <adam_rodriguez@bitbucket.org>
2017-10-26 00:25:05 -0700

    fix rest client

--- src/ProofingRestClient.php
+++ src/ProofingRestClient.php
@@ -46,17 +46,15 @@ class ProofingRestClient
         curl_setopt($_h, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($_h, CURLOPT_DNS_USE_GLOBAL_CACHE, false );
         curl_setopt($_h, CURLOPT_DNS_CACHE_TIMEOUT, 2 );
-
+        curl_setopt($_h, CURLINFO_HEADER_OUT, true);
         if(!((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443)) {
             curl_setopt($_h, CURLOPT_SSL_VERIFYPEER, 0);
             curl_setopt($_h, CURLOPT_SSL_VERIFYHOST, 0);
         }
         if("POST" === $method) {
-            curl_setopt($_h, CURLOPT_POST, 1);
-        }
-        if(count($params)) {
-            curl_setopt($_h, CURLOPT_POSTFIELDS, http_build_query($params));
-        }
+            curl_setopt($_h, CURLOPT_POST, count($params));
+            curl_setopt($_h, CURLOPT_POSTFIELDS, json_encode($params));
+        } 
 
         $resp = curl_exec($_h);
         curl_close($_h);
