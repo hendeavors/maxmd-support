2db7b5f0df14838e900f7822df708671e4b7fcad
Adam Rodriguez <adam_rodriguez@bitbucket.org>
2017-10-25 23:09:36 -0700

    add curl functionality

--- src/ProofingRestClient.php
+++ src/ProofingRestClient.php
@@ -6,6 +6,13 @@ class ProofingRestClient
 {
     private static $instance = null;
 
+    private $url;
+
+    private function __construct($url = null)
+    {
+        $this->url = $url;
+    }
+
     final private static function instance()
     {
         return static::$instance;
@@ -18,5 +25,41 @@ class ProofingRestClient
         }
 
         return static::instance();
+    }
+
+    public function Post($endpoint, $params, $headers = array())
+    {
+        return $this->request($endpoint, "POST", $params, $headers);
+    }
+
+    public function Get($endpoint, $params, $headers = array())
+    {
+        return $this->request($endpoint, "POST", $params, $headers);
+    }
+
+    protected function request($endpoint, $method = "GET", $params = array(), $headers = array())
+    {
+        $url = $this->url . $endpoint;
+        $_h = curl_init();
+        curl_setopt($_h, CURLOPT_URL, $url );
+        curl_setopt($_h, CURLOPT_HTTPHEADER,$headers);
+        curl_setopt($_h, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($_h, CURLOPT_DNS_USE_GLOBAL_CACHE, false );
+        curl_setopt($_h, CURLOPT_DNS_CACHE_TIMEOUT, 2 );
+
+        if(!((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443)) {
+            curl_setopt($_h, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($_h, CURLOPT_SSL_VERIFYHOST, 0);
+        }
+        if("POST" === $method) {
+            curl_setopt($_h, CURLOPT_POST, 1);
+        }
+        if(count($params)) {
+            curl_setopt($_h, CURLOPT_POSTFIELDS, $params);
+        }
+
+        $resp = curl_exec($_h);
+        curl_close($_h);
+        return $resp;
     }
 }
